8e5815abd6c4ec0e247466ce048e62b9b7dab646
Nour_Ayad <nour6666@gmail.com>
2016-12-13 20:35:18 -0800

    Blocks!
    Blocks have been added to the game, with a script to destroy them after
    hit a certain amount of times. BallController updated to do damage, and
    the beginnings of a score system and lives have been added with a new
    GameController.

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -7,7 +7,8 @@ public class BallController : MonoBehaviour
     public Transform StartPoint;
     private Transform ballTransform;
     private Rigidbody ballRigidBody;
-    public int speed;
+    public float speed;
+    public int damage;
 
     private Vector3 currentVelocity;
 
@@ -40,7 +41,9 @@ public class BallController : MonoBehaviour
             ballTransform.position = StartPoint.position;
         } else if (released && neverReleased) {
             //ballRigidBody.AddForce(0, 0, 100 * speed);
-            ballRigidBody.velocity = new Vector3(Random.Range(-3, 3) * speed, 0, 1 * speed);
+            ballRigidBody.velocity = new Vector3(Random.Range(-1.0f, 1.0f) * speed, 0, 1 * speed);
+            currentVelocity = ballRigidBody.velocity;
+            Debug.Log(currentVelocity.x);
             neverReleased = false;
         }
 
---
+++ Assets/Scripts/BlockDestroyer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockDestroyer : MonoBehaviour {
+
+    public GameController gameController;
+    public int blockLives;
+    private int blockHealth;
+    public int destroyScore;
+    public int hitScore;
+    
+
+	// Use this for initialization
+	void Start () {
+
+        blockHealth = blockLives;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (blockHealth == 0)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    void OnCollisionEnter (Collision other)
+    {
+        //if (other.tag == "GameBall")
+        {
+            //Damage(other.GetComponent<BallController>().damage);
+            Damage(1);
+            Debug.Log("damaged" + blockHealth);
+        }
+    }
+
+    void Damage (int dmg)
+    {
+        blockHealth -= dmg;
+        gameController.AddScore(hitScore);
+    }
+}
